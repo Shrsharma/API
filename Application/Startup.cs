53bbf21bf2af2d389397e31c016af31c96b8148d
shrsharma <shrsharma@HSC-6JVKLC2>
2021-04-16 11:38:10 +0530

    Commit

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -1,6 +1,8 @@
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient(typeof(IBaseRepo<>), typeof(BaseRepo<>));
+            services.AddDbContext<Data.AppContext>(item => item.UseSqlServer(Configuration.GetConnectionString("AppContext")));
             services.AddControllersWithViews();
         }
 
